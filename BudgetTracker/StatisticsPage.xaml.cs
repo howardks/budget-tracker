f223f1271a01dfb3ac287d3f1edf8214113cc49b
howardks <93079698+howardks@users.noreply.github.com>
2023-03-15 20:44:15 -0400

    Recommit

--- BudgetTracker/StatisticsPage.xaml.cs
+++ BudgetTracker/StatisticsPage.xaml.cs
@@ -13,7 +13,7 @@ using Windows.UI;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace BudgetTracker
+namespace BudgetTracker 
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
